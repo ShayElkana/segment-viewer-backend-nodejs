cdfa52e3cdde2a5ef1428d6ddc0260bbb6bb4772
ShayElkana <Shay.Elkana@Gmail.com>
2021-01-05 13:13:08 +0200

    Init

---
+++ server.js
@@ -0,0 +1,25 @@
+const routes = require('./routes')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 8061
+const ipAddress = '127.0.0.1'
+
+const server = require('http').Server(app)
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+routes(app)
+
+server.listen(port, ipAddress, (err) => {
+  if (err) {
+    console.error(err)
+  } else {
+    const baseUrl = `http://localhost:${port}`
+    console.log(`listening on ${port}, 
+    ${baseUrl}/segments
+    ${baseUrl}/segments/volumes`)
+  }
+})
